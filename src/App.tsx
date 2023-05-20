3b9a09a415dbb212fcf52f099f57776e5537f0e2
Juan Feris <feris.juan@gmail.com>
2023-05-20 10:35:42 -0500

    initial redux-toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,44 @@ import "./App.css";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 
+import { createSlice, configureStore } from '@reduxjs/toolkit'
+
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState: {
+    value: 0
+  },
+  reducers: {
+    incremented: state => {
+      state.value += 1
+    },
+    decremented: state => {
+      state.value -= 1
+    },
+    reset: state => {
+      state.value = 0
+    }
+  }
+})
+
+const { incremented, decremented, reset } = counterSlice.actions
+
+const store = configureStore({
+  reducer: counterSlice.reducer
+})
+
+// Can still subscribe to the store
+store.subscribe(() => console.log(store.getState()))
+
+// Still pass action objects to `dispatch`, but they're created for us
+store.dispatch(incremented())
+// {value: 1}
+store.dispatch(incremented())
+// {value: 2}
+store.dispatch(decremented())
+// {value: 1}
+store.dispatch(reset())
+
 function App() {
   const [count, setCount] = useState(0);
 
