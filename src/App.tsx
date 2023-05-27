197eea71b4b9ce1c99e2569fe954bb8e764e2f96
Juan Feris <feris.juan@gmail.com>
2023-05-27 11:40:16 -0500

    zustand store

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,45 @@
-import {useState} from "react";
+import { shallow } from 'zustand/shallow';
 import "./App.css";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useUserStore } from './store';
+import { useBearStore, useUserStore2 } from './store/store';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [firstName, updateFirstName] = useUserStore(
+    (state) => [state.firstName, state.updateFirstName],
+    shallow
+  );
+
+  // with selectors
+  const firstName2 = useUserStore2.use.firstName();
+
+  const bears = useBearStore.use.bears();
+  const increase = useBearStore.use.increase();
+  const increment = useBearStore.use.increment();
 
   return (
-    <>
+    <main>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
+        <p>{bears} bears</p>
+        <button onClick={() => increase(5)}>increase</button>
+        <button onClick={() => increment()}>increment</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <label>
+        First name
+        <input
+          onChange={(e) => updateFirstName(e.currentTarget.value)}
+          value={firstName}
+        />
+      </label>
+
+      <p>
+        Hello, <strong>{firstName}!</strong>
       </p>
-    </>
+
+      <p>
+        Hello, <strong>{firstName2}!</strong>
+      </p>
+    </main>
   );
 }
 
---
+++ src/store/store.ts
@@ -0,0 +1,49 @@
+import { create, StoreApi, UseBoundStore } from 'zustand'
+
+type State = {
+  firstName: string
+  lastName: string
+}
+
+type Action = {
+  updateFirstName: (firstName: State['firstName']) => void
+  updateLastName: (lastName: State['lastName']) => void
+}
+
+export const useUserStore = create<State & Action>((set) => ({
+  firstName: '',
+  lastName: '',
+  updateFirstName: (firstName) => set(() => ({ firstName: firstName })),
+  updateLastName: (lastName) => set(() => ({ lastName: lastName })),
+}))
+
+type WithSelectors<S> = S extends { getState: () => infer T }
+  ? S & { use: { [K in keyof T]: () => T[K] } }
+  : never
+
+const createSelectors = <S extends UseBoundStore<StoreApi<object>>>(
+  _store: S
+) => {
+  const store = _store as WithSelectors<typeof _store>
+  store.use = {}
+  for (const k of Object.keys(store.getState())) {
+    (store.use as any)[k] = () => store((s) => s[k as keyof typeof s])
+  }
+
+  return store
+}
+
+interface BearState {
+  bears: number
+  increase: (by: number) => void
+  increment: () => void
+}
+
+const useBearStoreBase = create<BearState>()((set) => ({
+  bears: 0,
+  increase: (by) => set((state) => ({ bears: state.bears + by })),
+  increment: () => set((state) => ({ bears: state.bears + 1 })),
+}))
+
+export const useBearStore = createSelectors(useBearStoreBase)
+export const useUserStore2 = createSelectors(useUserStore)
