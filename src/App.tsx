8345f89fc3a181b40df2f444a8e183ffb4571252
Juan Feris <feris.juan@gmail.com>
2023-06-10 10:50:20 -0500

    basic usage

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,33 @@
-import {useState} from "react";
-import "./App.css";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import {ChangeEvent} from 'react'
+import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0);
+import {atom, useAtom} from 'jotai'
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+// Create your atoms and derivatives
+const textAtom = atom('')
+const uppercaseAtom = atom((get) => get(textAtom).toUpperCase())
+
+// Use them anywhere in your app
+const Input = () => {
+  const [text, setText] = useAtom(textAtom)
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => setText(e.target.value)
+
+  return <input value={text} onChange={handleChange} />
 }
 
-export default App;
+const Uppercase = () => {
+  const [uppercase] = useAtom(uppercaseAtom)
+  return <div>Uppercase: {uppercase}</div>
+}
+
+function App() {
+  return (
+    <>
+      <Input />
+      <Uppercase />
+    </>
+  )
+}
+
+export default App
